e0dd6b7e9a813da83cf5ea2f36fe2e4bb31f1f5e
sujitv19196 <sujit5817@gmail.com>
2023-11-10 12:01:33 -0600

    get captions to display

--- src/lib/components/CaptionedVideo.svelte
+++ src/lib/components/CaptionedVideo.svelte
@@ -23,22 +23,52 @@
     })
 </script>
 
-<video
-    id="videoPlayer"
-    hidden={hidden}
-    controls
-    src={videoSrc}
-    bind:this={video}
-    bind:currentTime
-    bind:paused
-    bind:ended
-    bind:volume
-/>
+<div class="video-container">
+    <video
+        id="videoPlayer"
+        hidden={hidden}
+        controls
+        src={videoSrc}
+        bind:this={video}
+        bind:currentTime
+        bind:paused
+        bind:ended
+        bind:volume
+    />
+    <div class="captions-container">
+        {#if !_captions.length}
+            <p>No captions available</p>
+        {:else}
+            {#each _captions as caption}
+                <div class="caption">{caption.text}</div>
+            {/each}
+        {/if}
+    </div>
+</div>
+
+<button>asdfasdf</button>
 
 <style>
+    .video-container {
+        display: flex;
+    }
+
     #videoPlayer {
-        width: calc(100% - 20px);
+        width: calc(70% - 20px);
         margin: 10px;
         border-radius: 10px;
+    }
+
+    .captions-container {
+        width: 30%;
+        margin: 10px;
+        padding: 10px;
+        border-radius: 10px;
+        overflow-y: auto;
+        background-color: #f0f0f0;
+    }
+
+    .caption {
+        margin-bottom: 10px;
     }
 </style>
